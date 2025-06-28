afa9cbe7991ce8b329eddd02ae89f7ac226977c3
trishaladabala <adabalatrishal@gmail.com>
2025-06-28 22:24:28 +0530

    Fresh start

---
+++ make_style.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if ! [ -x "$(command -v sass)" ]; then
+  echo 'Error: sass is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v postcss)" ]; then
+  echo 'Error: postcss is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v autoprefixer)" ]; then
+  echo 'Error: autoprefixer is not installed.' >&2
+  exit 1
+fi
+
+cd "$(dirname "$0")" || exit
+
+build_style() {
+  echo "Creating $1 style..."
+  cp resources/vars-$1.scss resources/vars.scss
+  sass resources:sass_processed
+  postcss sass_processed/style.css sass_processed/martor-description.css --verbose --use autoprefixer -d $2
+}
+
+build_style 'default' 'resources'
+build_style 'dark' 'resources/dark'
